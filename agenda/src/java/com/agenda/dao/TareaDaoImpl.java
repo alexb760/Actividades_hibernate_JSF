5e5c1dffda8665d127eccb93b2a51aeab35a799b
anfelipe <anfelipecardenas@hotmail.com>
2015-03-24 16:53:56 -0500

    Configuración consultas tarea.

--- agenda/src/java/com/agenda/dao/TareaDaoImpl.java
+++ agenda/src/java/com/agenda/dao/TareaDaoImpl.java
@@ -5,8 +5,11 @@
  */
 package com.agenda.dao;
 
+import java.util.*;
 import com.agenda.model.Tarea;
 import java.io.Serializable;
+import org.hibernate.HibernateException;
+import util.HibernateUtil;
 
 /**
  *
@@ -14,4 +17,50 @@ import java.io.Serializable;
  */
 public class TareaDaoImpl extends GenericDaoImpl<Tarea, Integer> implements TareaDao{
     
+    public List <Tarea> getTareaHoy(){
+        
+        List <Tarea> list;
+        try{
+            java.util.Date fecha = new Date();
+            session = HibernateUtil.getSession();
+            tx = session.beginTransaction();
+            list = session.createQuery("from Tarea as ta where ta.fecha >= 2015-03-24").list();
+            tx.commit();
+        }catch(HibernateException e){
+            list = null;
+            tx.rollback();
+            throw e;
+        }
+        return list;
+    }
+    
+    public List<Tarea> getPendientes(){
+        List<Tarea> list;
+        try{
+            session = HibernateUtil.getSession();
+            tx = session.beginTransaction();
+            list = session.createQuery("from Tarea as ta where ta.estadoTarea = True").list();
+            tx.commit();
+        }catch(HibernateException e){
+            list = null;
+            tx.rollback();
+            throw e;
+        }
+        return list;
+    }
+    
+    public List<Tarea> getProyectos(){
+        List<Tarea> list;
+        try{
+            session = HibernateUtil.getSession();
+            tx = session.beginTransaction();
+            list = session.createQuery("from Tarea as ta where ta.estadoTarea = True").list();
+            tx.commit();
+        }catch(HibernateException e){
+            list = null;
+            tx.rollback();
+            throw e;
+        }
+        return list;
+    }
 }
